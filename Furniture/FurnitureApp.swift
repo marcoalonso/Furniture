60fdf91d80912be9441fc826138f9464b0f3de55
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2024-01-07 10:23:35 -0600

    Getting Started

---
+++ Furniture/FurnitureApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureApp.swift
+//  Furniture
+//
+//  Created by Marco Alonso Rodriguez on 07/01/24.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
